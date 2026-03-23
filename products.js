36e3f41dd38a3ac2b37947d451fdac1ec8f0f85e
Emma Schink <schinkdesigns@gmail.com>
2026-03-23 13:54:26 -0400

    Create products.js

---
+++ products.js
@@ -0,0 +1,43 @@
+const PRODUCTS = {
+  forest: {
+    name: "Forest Creatures Journal",
+    price: 28.00,
+    images: [
+      "/images/fc1.png",
+      "/images/fc2.png",
+      "/images/fc3.png",
+      "/images/fc4.png",
+      "/images/fc5.png",
+      "/images/fc6.png"
+    ],
+    colors: [
+      { name: "Bunny", image: "/images/fcp1.png" },
+      { name: "Raccoon", image: "/images/fcp2.png" },
+      { name: "Mouse", image: "/images/fcp3.png" },
+      { name: "Deer", image: "/images/fcp4.png" }
+    ],
+    description: "Take notes in a whimsical way with our Forest Creatures journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  wildflowers: {
+    name: "Bold Wildflowers Journal",
+    price: 28.00,
+    images: [
+      "/images/wf1.png",
+      "/images/wf2.png",
+      "/images/wf3.png",
+      "/images/wf4.png",
+      "/images/wf5.png",
+      "/images/wf6.png"
+    ],
+    colors: [
+      { name: "Yellow", image: "/images/wfp1.png" },
+      { name: "Blue", image: "/images/wfp2.png" },
+      { name: "Green", image: "/images/wfp3.png" },
+      { name: "Pink", image: "/images/wfp4.png" }
+    ],
+    description: "Take notes in a whimsical way with our Bold Wildflowers journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
