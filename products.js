37af921a79d698693643aef07f174e45d74e5608
Emma Schink <schinkdesigns@gmail.com>
2026-03-23 15:13:23 -0400

    Update products.js

--- products.js
+++ products.js
@@ -8,7 +8,8 @@ const PRODUCTS = {
       "/images/fc3.png",
       "/images/fc4.png",
       "/images/fc5.png",
-      "/images/fc6.png"
+      "/images/fc6.png",
+      "/images/fc7.png"
     ],
     colors: [
       { name: "Bunny", image: "/images/fcp1.png" },
@@ -17,6 +18,61 @@ const PRODUCTS = {
       { name: "Deer", image: "/images/fcp4.png" }
     ],
     description: "Take notes in a whimsical way with our Forest Creatures journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  roses: {
+    name: "Vintage Roses Journal",
+    price: 28.00,
+    images: [
+      "/images/vr1.png",
+      "/images/vr2.png",
+      "/images/vr3.png",
+      "/images/vr4.png",
+      "/images/vr5.png",
+      "/images/vr6.png",
+      "/images/vr7.png"
+    ],
+    colors: [
+      { name: "Green", image: "/images/vrp1.png" },
+      { name: "Red", image: "/images/vrp2.png" },
+      { name: "Brown", image: "/images/vrp3.png" },
+      { name: "Beige", image: "/images/vrp4.png" }
+    ],
+    description: "Take notes in a whimsical way with our Bold Wildflowers journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  birthflower: {
+    name: "Birth Flower Journal",
+    price: 28.00,
+    images: [
+      "/images/bf1.png",
+      "/images/bf2.png",
+      "/images/bf3.png",
+      "/images/bf4.png",
+      "/images/bf5.png",
+      "/images/bf6.png",
+      "/images/bf7.png",
+      "/images/bf8.png"
+    ],
+    colors: [
+      { name: "January", image: "/images/bfp1.png" },
+      { name: "February", image: "/images/bfp2.png" },
+      { name: "March", image: "/images/bfp3.png" },
+      { name: "April", image: "/images/bfp4.png" },
+      { name: "May", image: "/images/bfp5.png" },
+      { name: "June", image: "/images/bfp6.png" },
+      { name: "July", image: "/images/bfp7.png" },
+      { name: "August", image: "/images/bfp8.png" },
+      { name: "September", image: "/images/bfp9.png" },
+      { name: "October", image: "/images/bfp10.png" },
+      { name: "November", image: "/images/bfp11.png" },
+      { name: "December", image: "/images/bfp12.png" },
+    ],
+    description: "Take notes in a whimsical and personal way with our Birth Flower journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter! Plus, these make perfectly personalized bachelorette or birthday gifts for sisters, friends, or soulmates!"
   }
 };
 
@@ -39,5 +95,65 @@ const PRODUCTS = {
       { name: "Pink", image: "/images/wfp4.png" }
     ],
     description: "Take notes in a whimsical way with our Bold Wildflowers journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  bohemian: {
+    name: "Bohemian Sun Journal",
+    price: 28.00,
+    images: [
+      "/images/bs1.png",
+      "/images/bs2.png",
+      "/images/bs3.png",
+      "/images/bs4.png",
+      "/images/bs5.png",
+      "/images/bs6.png"
+    ],
+    colors: [
+      { name: "Light", image: "/images/bsp1.png" },
+      { name: "Dark", image: "/images/bsp2.png" },
+    ],
+    description: "Take notes in a whimsical way with our Bohemian Sun journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  florals: {
+    name: "Coquette Floral Journal",
+    price: 28.00,
+    images: [
+      "/images/cf1.png",
+      "/images/cf2.png",
+      "/images/cf3.png",
+      "/images/cf4.png",
+      "/images/cf5.png",
+      "/images/cf6.png"
+    ],
+    colors: [
+      { name: "Light", image: "/images/cfp1.png" },
+      { name: "Dark", image: "/images/cfp2.png" },
+    ],
+    description: "Take notes in a whimsical way with our Coquette Floral journal. Packed with beautiful designs and simple lined pages, the freedom is yours to capture what makes your heart flutter!"
+  }
+};
+
+const PRODUCTS = {
+  botanical: {
+    name: "Botanical Weekly Planner",
+    price: 32.00,
+    images: [
+      "/images/bp1.png",
+      "/images/bp2.png",
+      "/images/bp3.png",
+      "/images/bp4.png",
+      "/images/bp5.png",
+      "/images/bp6.png"
+    ],
+    colors: [
+      { name: "Green", image: "/images/bpp1.png" },
+      { name: "Beige", image: "/images/bpp2.png" },
+    ],
+    description: "Stay organized and stylish with the Botanical Planner! With two color options, monthly calenders, book trackers, habit pages, weekly trackers, and meal plans, this 176 page planner is the perfect pocket-sized companion to bring on the go!"
   }
 };
