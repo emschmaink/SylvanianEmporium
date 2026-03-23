3f201ca0914796b239bbc23f4f9fb50eb3ff320c
Emma Schink <schinkdesigns@gmail.com>
2026-03-23 13:38:51 -0400

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,21 @@
+const PRODUCTS = {
+  forest: {
+    name: "Forest Creatures Journal",
+    price: 28.00,
+    images: [
+      "/images/fc1.png",
+      "/images/fc2.png",
+      "/images/fc3.png",
+      "/images/fc4.png",
+      "/images/fc5.png",
+      "/images/fc6.png"
+    ],
+    colors: [
+      { name: "Bunny", image: "/images/fcp1.png" },
+      { name: "Raccoon", image: "/images/fcp2.png" },
+      { name: "Mouse", image: "/images/fcp3.png" },
+      { name: "Deer", image: "/images/fcp4.png" }
+    ],
+    description: "Take notes in a whimsical way with our Forest Creatures journal..."
+  }
+};
